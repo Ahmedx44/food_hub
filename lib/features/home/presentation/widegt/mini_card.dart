f91cca7171b1f3f74589cfb0292ba12d30ee1a79
Ahmed Gemechu <ahmedgemechu14@gmail.com>
2024-10-18 07:16:26 +0300

    "Added Firestore import and functionality to add food data to Firestore, modified MiniCards widget layout and styling, and updated image URLs and names."

--- lib/features/home/presentation/widegt/mini_card.dart
+++ lib/features/home/presentation/widegt/mini_card.dart
@@ -11,16 +11,21 @@ class MiniCards extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(MediaQuery.of(context).size.width * 0.03),
-      height: 200,
-      width: 200,
+      margin: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * 0.02,
+          vertical: MediaQuery.of(context).size.height * 0.03),
+      height: 130,
+      width: 150,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
           color: Theme.of(context).colorScheme.secondary),
       child: Column(
         children: [
-          ExtendedImage.network(height: 180, cache: true, image),
-          Text(name)
+          ExtendedImage.network(height: 139, cache: true, image),
+          Text(
+            name,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+          )
         ],
       ),
     );
